f772afd1bf94b5da87328ded11b7241688c1a983
joseph10088 <104082003+joseph10088@users.noreply.github.com>
2024-08-02 04:29:11 +0200

    script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('aggiungiBtn').addEventListener('click', function() {
+    alert('Pulsante AGGIUNGI premuto!');
+});
+
+document.getElementById('espletatiBtn').addEventListener('click', function() {
+    alert('Pulsante ESPLETATI premuto!');
+});
+
+document.getElementById('sospesiBtn').addEventListener('click', function() {
+    alert('Pulsante SOSPESI premuto!');
+});
+
+document.getElementById('convalidaBtn').addEventListener('click', function() {
+    alert('Pulsante CONVALIDA premuto!');
+});
